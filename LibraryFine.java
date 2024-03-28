78bb0ab2dec0304cdea7eefea87bbae1430a647b
VENKATA THRILOK KOTAPATI <136972232+thrilok2612@users.noreply.github.com>
2024-03-28 17:52:19 +0530

    LibraryFine.java

--- LibraryFine.java
+++ LibraryFine.java
@@ -3,20 +3,23 @@ import java.util.Scanner;
 public class LibraryFine {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
-        System.out.print("Enter the number of days late: ");
+
+        System.out.print("Enter the number of days the member is late to return the book: ");
         int daysLate = scanner.nextInt();
-        
+
         if (daysLate <= 7) {
-            System.out.println("Fine: 50 paise");
+            double fine = daysLate * 0.5;
+            System.out.println("Fine for being " + daysLate + " days late is: " + fine + " rupees.");
         } else if (daysLate <= 14) {
-            System.out.println("Fine: Rs. 1/-");
+            double fine = (7 * 0.5) + ((daysLate - 7) * 1);
+            System.out.println("Fine for being " + daysLate + " days late is: " + fine + " rupees.");
         } else if (daysLate <= 21) {
-            System.out.println("Fine: Rs. 5/-");
+            double fine = (7 * 0.5) + (7 * 1) + ((daysLate - 14) * 5);
+            System.out.println("Fine for being " + daysLate + " days late is: " + fine + " rupees.");
         } else {
-            System.out.println("Membership canceled.");
+            System.out.println("Your membership is canceled due to returning the book more than 21 days late.");
         }
-        
+
         scanner.close();
     }
 }
